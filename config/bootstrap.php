acbab41c3ae34ffa03ef199b9ae05bdc5ff6a746
d1rk <d1rk@gmx.de>
2012-02-03 18:20:26 +0100

    adding basic structure

---
+++ config/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+use lithium\net\http\Router;
+
+Router::connect('/li3_bootstrap/{:path:js|css}/{:file}.{:type}', array(), function($request) {
+	$req = $request->params;
+	$file = dirname(__DIR__) . "/webroot/{$req['path']}/{$req['file']}.{$req['type']}";
+
+	if (!file_exists($file)) {
+		return;
+	}
+
+	return new Response(array(
+		'body' => file_get_contents($file),
+		'headers' => array('Content-type' => str_replace(
+			array('css', 'js'), array('text/css', 'text/javascript'), $req['type']
+		))
+	));
+});
