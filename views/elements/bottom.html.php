0ab3291c247bc69520bda950844611cdd65d4d61
d1rk <d1rk@gmx.de>
2013-02-26 13:04:28 +0100

    speedup asset url generation in bottom element

--- views/elements/bottom.html.php
+++ views/elements/bottom.html.php
@@ -1,26 +1,14 @@
 <script type="text/javascript" charset="utf-8">
 head.js(
-	{ jquery: "<?php echo $this->url('/li3_bootstrap/js/jquery.min.js'); ?>"},
-	{ bootstrap: "<?php echo $this->url('/li3_bootstrap/js/bootstrap.min.js'); ?>"},
-	{ moment: "<?php echo $this->url('/li3_bootstrap/js/moment.min.js'); ?>"},
-	{ icanhaz: "<?php echo $this->url('/li3_bootstrap/js/icanhaz.min.js'); ?>"},
-	{ jeditable: "<?php echo $this->url('/li3_bootstrap/js/jeditable.min.js'); ?>"},
-	{ highlight: "<?php echo $this->url('/li3_bootstrap/js/highlight.min.js'); ?>"},
-	{ slug: "<?php echo $this->url('/li3_bootstrap/js/jquery.slug.js'); ?>"},
-	{ formParams: "<?php echo $this->url('/li3_bootstrap/js/jquery.formParams.js'); ?>"},
-	{ tools: "<?php echo $this->url('/li3_bootstrap/js/tools.js'); ?>"},
-	{ app: "<?php echo $this->url('/js/app.js'); ?>"}
+	{ jquery: "<?php echo $this->path('/li3_bootstrap/js/jquery.min.js'); ?>"},
+	{ bootstrap: "<?php echo $this->path('/li3_bootstrap/js/bootstrap.min.js'); ?>"},
+	{ moment: "<?php echo $this->path('/li3_bootstrap/js/moment.min.js'); ?>"},
+	{ icanhaz: "<?php echo $this->path('/li3_bootstrap/js/icanhaz.min.js'); ?>"},
+	{ jeditable: "<?php echo $this->path('/li3_bootstrap/js/jeditable.min.js'); ?>"},
+	{ highlight: "<?php echo $this->path('/li3_bootstrap/js/highlight.min.js'); ?>"},
+	{ slug: "<?php echo $this->path('/li3_bootstrap/js/jquery.slug.js'); ?>"},
+	{ formParams: "<?php echo $this->path('/li3_bootstrap/js/jquery.formParams.js'); ?>"},
+	{ tools: "<?php echo $this->path('/li3_bootstrap/js/tools.js'); ?>"},
+	{ app: "<?php echo $this->path('/js/app.js'); ?>"}
 );
 </script>
-<?php /*
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-XXXXX-Y']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-</script>
-*/ ?>
