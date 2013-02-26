8b311b76ed62a621d55cbe1ef1a6af1d2c0f6463
d1rk <d1rk@gmx.de>
2013-02-26 13:03:20 +0100

    allow markup to be shown in TabHelper

--- views/mustache/tabs.html.php
+++ views/mustache/tabs.html.php
@@ -1,6 +1,6 @@
 <ul class="nav{{^class}} nav-tabs{{/class}}{{#class}} {{ class }}{{/class}}{{#active}} active{{/active}}"{{#id}} id="{{ id }}"{{/id}}>
 	{{#right}}
-		<li class="pull-right">{{ right }}</li>
+		<li class="pull-right">{{{ right }}}</li>
 	{{/right}}
 	{{#tabs}}
 		<li class="{{#class}}{{ class }}{{/class}}{{#active}} active{{/active}}">
@@ -22,7 +22,7 @@
 <div class="tab-content">
 	{{#panes}}
 		<div class="tab-pane{{#class}} {{ class }}{{/class}}{{#active}} active{{/active}}"{{#id}} id="{{ id }}"{{/id}}>
-			{{ content }}
+			{{{ content }}}
 		</div>
 	{{/panes}}
 </div>
