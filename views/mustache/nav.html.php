cebc13a962759baaf82adf847b22d6a8f076fff5
d1rk <d1rk@gmx.de>
2013-03-11 10:55:13 +0100

    allow navigations to have children

--- views/mustache/nav.html.php
+++ views/mustache/nav.html.php
@@ -1,18 +1,35 @@
 <ul class="{{^class}}nav{{/class}}{{#class}}{{ class }}{{/class}}{{#active}} active{{/active}}"{{#id}} id="{{ id }}"{{/id}}>
 	{{#items}}
-		<li class="{{#class}} {{ class }}{{/class}}{{#active}} active{{/active}}">
+		<li class="{{#class}} {{ class }}{{/class}}{{#active}} active{{/active}}{{#children}} dropdown{{/children}}">
 			{{#url}}
-				<a href="{{ url }}"{{#tab}} data-toggle="tab"{{/tab}}>
+				<a href="{{ url }}"{{#tab}} data-toggle="tab"{{/tab}}{{#children}} data-toggle="dropdown" class="dropdown-toggle"{{/children}}>
 			{{/url}}
 				{{#icon}}
 					<i class="icon-{{ icon }}"></i>
 				{{/icon}}
-				{{#name}}
-					{{ name }}
-				{{/name}}
+				{{#name}}{{ name }}{{/name}}
+				{{#children}} <b class="caret"></b>{{/children}}
 			{{#url}}
 				</a>
 			{{/url}}
+			{{#children}}
+				<ul class="dropdown-menu" role="menu">
+					{{#items}}
+						<li class="{{#class}} {{ class }}{{/class}}{{#active}} active{{/active}}">
+							{{#url}}
+								<a href="{{ url }}">
+							{{/url}}
+								{{#icon}}
+									<i class="icon-{{ icon }}"></i>
+								{{/icon}}
+								{{#name}}{{ name }}{{/name}}
+							{{#url}}
+								</a>
+							{{/url}}
+						</li>
+					{{/items}}
+				</ul>
+			{{/children}}
 		</li>
 	{{/items}}
 </ul>
