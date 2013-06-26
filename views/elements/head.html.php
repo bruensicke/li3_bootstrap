cf07220ffe00658123ab19de5516f3cb0ee88ee8
d1rk <d1rk@gmx.de>
2013-06-26 14:43:18 +0200

    load styles within head element

--- views/elements/head.html.php
+++ views/elements/head.html.php
@@ -11,6 +11,7 @@
 <?php echo $this->html->script('/li3_bootstrap/js/head.js'); ?>
 <?php echo $this->head(); ?>
 <?php echo $this->scripts(); ?>
+<?php echo $this->styles(); ?>
 <?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 
 <meta name="MSSmartTagsPreventParsing" content="true" />
