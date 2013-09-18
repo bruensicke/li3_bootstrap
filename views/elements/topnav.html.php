d516af04a9b2d14ec1067b858c181db598c0dc5a
d1rk <d1rk@gmx.de>
2013-09-18 12:01:19 +0200

    update topnav element to comply with new bootstrap css classes

--- views/elements/topnav.html.php
+++ views/elements/topnav.html.php
@@ -1,27 +1,29 @@
-<div class="navbar navbar-fixed-top">
-	<div class="navbar-inner">
-		<div class="container">
+<nav class="navbar navbar-default block" role="navigation">
+	<div class="navbar-header">
 
-			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</a>
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
 
-			<a href="<?=$this->url('/'); ?>" class="pull-left">
-				<?=$this->html->image('logo.png', array('style' => 'padding-right: 10px')); ?>
-			</a>
+		<a href="<?=$this->url('/'); ?>" class="pull-left">
+			<?=$this->html->image('logo.png', array('style' => 'padding-right: 10px')); ?>
+		</a>
+		<a href="<?=$this->url('/'); ?>" class="brand">
+			<span>Home</span>
+		</a>
 
-			<a href="<?=$this->url('/'); ?>" class="brand">
-				<span>Home</span>
-			</a>
-
-			<div class="nav-collapse">
-				<ul class="nav">
-					<li><?php echo $this->html->link('Start', '/'); ?></li>
-				</ul>
-			</div>
-
-		</div>
 	</div>
-</div>
+
+	<div class="collapse navbar-collapse navbar-ex1-collapse">
+
+		<div class="navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li><?php echo $this->html->link('Start', '/'); ?></li>
+			</ul>
+		</div>
+
+	</div>
+</nav>
